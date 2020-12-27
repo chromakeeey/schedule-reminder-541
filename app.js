e2418476f5a4f2d94176521e308cefa3e44f0558
chromakeeey <chromakeeey@gmail.com>
2020-12-27 23:52:34 +0200

    update week schedule

--- app.js
+++ app.js
@@ -33,40 +33,40 @@ const lessonsSunday = [];
 
 // subota
 const lessonsSaturday = [
-    { timeStart: [9, 0], timeEnd: [10, 20], name: [names.lan] },
-    { timeStart: [10, 35], timeEnd: [11, 55], name: [names.mov] },
-    { timeStart: [12, 10], timeEnd: [13, 30], name: [names.web] }
+    // { timeStart: [9, 0], timeEnd: [10, 20], name: [names.lan] },
+    // { timeStart: [10, 35], timeEnd: [11, 55], name: [names.mov] },
+    // { timeStart: [12, 10], timeEnd: [13, 30], name: [names.web] }
 ];
 
 const lessonsMonday = [
-    { timeStart: [8, 30], timeEnd: [9, 50], name: [names.met] },
-    { timeStart: [10, 5], timeEnd: [11, 25], name: [groupLesson(names.db, 1), groupLesson(names.db, 2)] },
-    { timeStart: [12, 0], timeEnd: [13, 20], name: [groupLesson(names.web, 1), groupLesson(names.pract, 2)] },
-    { timeStart: [13, 35], timeEnd: [14, 55], name: [groupLesson(names.pract, 1), groupLesson(names.web, 2)] },
-    //{ timeStart: [15, 10], timeEnd: [16, 30], name: [groupLesson(names.met, 2)] }
+    { timeStart: [8, 30], timeEnd: [9, 50], name: [groupLesson(names.mobile, 1)] },
+    { timeStart: [10, 5], timeEnd: [11, 25], name: [groupLesson(names.web, 1), groupLesson(names.net, 2)] },
+    { timeStart: [12, 0], timeEnd: [13, 20], name: [names.lan] },
+    { timeStart: [13, 35], timeEnd: [14, 55], name: [groupLesson(names.net, 1), groupLesson(names.web, 2)] },
+    { timeStart: [15, 10], timeEnd: [16, 30], name: [groupLesson(names.mobile, 2)] }
 ]
 
 const lessonsTuesday = [
-    { timeStart: [8, 30], timeEnd: [9, 50], name: [groupLesson(names.mobile, 1)] },
-    { timeStart: [10, 5], timeEnd: [11, 25], name: [groupLesson(names.mobile, 1), groupLesson(names.pract, 2)] },
-    { timeStart: [12, 0], timeEnd: [13, 20], name: [names.lan] },
-    { timeStart: [13, 35], timeEnd: [14, 55], name: [groupLesson(names.pract, 1), groupLesson(names.mobile, 2)] },
-    { timeStart: [15, 10], timeEnd: [16, 30], name: [groupLesson(names.mobile, 2)] },
+    { timeStart: [8, 30], timeEnd: [9, 50], name: [groupLesson(names.met, 1)] },
+    { timeStart: [10, 5], timeEnd: [11, 25], name: [groupLesson(names.web, 1), groupLesson(names.net, 2)] },
+    { timeStart: [12, 0], timeEnd: [13, 20], name: [names.economic] },
+    { timeStart: [13, 35], timeEnd: [14, 55], name: [groupLesson(names.net, 1), groupLesson(names.web, 2)] },
+    { timeStart: [15, 10], timeEnd: [16, 30], name: [groupLesson(names.met, 2)] },
 ]
 
 const lessonsWednesday = [
-    { timeStart: [8, 30], timeEnd: [9, 50], name: [groupLesson(names.pract, 1), groupLesson(names.engsoft, 2)] },
-    { timeStart: [10, 5], timeEnd: [11, 25], name: [groupLesson(names.engsoft, 1), groupLesson(names.pract, 2)] },
-    { timeStart: [12, 0], timeEnd: [13, 20], name: [names.web] },
+    { timeStart: [8, 30], timeEnd: [9, 50], name: [names.lan] },
+    { timeStart: [10, 5], timeEnd: [11, 25], name: [groupLesson(names.web, 1), groupLesson(names.net, 2)] },
+    { timeStart: [12, 0], timeEnd: [13, 20], name: [groupLesson(names.net, 1), groupLesson(names.web, 2)] },
     { timeStart: [13, 35], timeEnd: [14, 55], name: [names.economic] },
     //{ timeStart: [15, 10], timeEnd: [16, 30], name: [groupLesson(names.web, 2)] },
 ];
 
 const lessonsThursday = [
-    { timeStart: [8, 30], timeEnd: [9, 50], name: [names.economic] },
-    { timeStart: [10, 5], timeEnd: [11, 25], name: [groupLesson(names.pract, 1), groupLesson(names.web, 2)] },
-    { timeStart: [12, 0], timeEnd: [13, 20], name: [groupLesson(names.web, 1), groupLesson(names.pract, 2)] },
-    { timeStart: [13, 35], timeEnd: [14, 55], name: [names.vih] },
+    // { timeStart: [8, 30], timeEnd: [9, 50], name: [names.economic] },
+    // { timeStart: [10, 5], timeEnd: [11, 25], name: [groupLesson(names.pract, 1), groupLesson(names.web, 2)] },
+    // { timeStart: [12, 0], timeEnd: [13, 20], name: [groupLesson(names.web, 1), groupLesson(names.pract, 2)] },
+    // { timeStart: [13, 35], timeEnd: [14, 55], name: [names.vih] },
     //{ timeStart: [15, 10], timeEnd: [16, 10], name: [names.vih] },
 ]
 
